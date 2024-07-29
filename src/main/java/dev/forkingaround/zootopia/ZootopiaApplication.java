85c7d076d876204deb3cfd2e0abe46d04790637f
Miharu669 <veronicadoelfuentes@gmail.com>
2024-07-29 15:11:54 +0200

    first commit

---
+++ src/main/java/dev/forkingaround/zootopia/ZootopiaApplication.java
@@ -0,0 +1,13 @@
+package dev.forkingaround.zootopia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZootopiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZootopiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/forkingaround/zootopia/ZootopiaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.forkingaround.zootopia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZootopiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
