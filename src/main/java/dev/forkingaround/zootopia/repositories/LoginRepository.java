6a1402245068280973f28e2306871ffdbe824d06
Miharu669 <veronicadoelfuentes@gmail.com>
2024-08-06 14:18:42 +0200

    login

--- src/main/java/dev/forkingaround/zootopia/repositories/LoginRepository.java
+++
@@ -1,8 +0,0 @@
-package dev.forkingaround.zootopia.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import dev.forkingaround.zootopia.models.User;
-
-public interface LoginRepository extends JpaRepository<User, Long> {
-}
---
+++ src/main/java/dev/forkingaround/zootopia/repositories/RoleRepository.java
@@ -0,0 +1,8 @@
+package dev.forkingaround.zootopia.repositories;
+
+import dev.forkingaround.zootopia.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long>{
+    
+}
