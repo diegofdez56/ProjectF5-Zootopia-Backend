77e3ac0a3e1d0f53ae5d182dbf992460d73b958c
Bego Blanco <begoblanfer@gmail.com>
2024-08-09 22:17:47 +0200

    checkin problematic stuf

--- src/main/java/dev/forkingaround/zootopia/config/SecurityConfig.java
+++ src/main/java/dev/forkingaround/zootopia/config/SecurityConfig.java
@@ -38,35 +38,59 @@ public class SecurityConfig {
         public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
                 http
-                        .cors(withDefaults())
-                        .csrf(csrf -> csrf.disable())
-                        .formLogin(form -> form.disable())
-                        .logout(out -> out
-                                .logoutUrl(endpoint + "/logout")
-                                .deleteCookies("JSESSIONID"))
-                        .authorizeHttpRequests(auth -> auth
-                                .requestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**")).permitAll()
-                                .requestMatchers( HttpMethod.POST, endpoint + "/register").permitAll()
-                                .requestMatchers(HttpMethod.GET, endpoint + "/login").hasAnyRole("USER","ADMIN")
-                                .requestMatchers(HttpMethod.GET, endpoint + "/countries").hasAnyRole("USER", "ADMIN")
-                                .requestMatchers(HttpMethod.POST, endpoint + "/countries").hasRole("ADMIN")
-                                .anyRequest().authenticated())
-                        .userDetailsService(jpaUserDetailsService)
-                        .httpBasic(withDefaults())
-                        .sessionManagement(session -> session
-                                .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED));
+                                .cors(withDefaults())
+                                .csrf(csrf -> csrf.disable())
+                                .formLogin(form -> form.disable())
+                                .logout(out -> out
+                                                .logoutUrl(endpoint + "/logout")
+                                                .deleteCookies("JSESSIONID"))
+                                .authorizeHttpRequests(auth -> auth
+                                                .requestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**"))
+                                                .permitAll()
+                                                .requestMatchers(HttpMethod.POST, endpoint + "/register").permitAll()
+                                                .requestMatchers(HttpMethod.GET, endpoint + "/login")
+                                                .hasAnyRole("USER", "ADMIN")
+
+                                                .requestMatchers(HttpMethod.GET, endpoint + "/animals/all").permitAll()
+                                                .requestMatchers(HttpMethod.POST, endpoint + "/animals/add")
+                                                .hasRole("ADMIN")
+                                                .requestMatchers(HttpMethod.PUT, endpoint + "/animals/update/{id}")
+                                                .hasRole("ADMIN")
+                                                .requestMatchers(HttpMethod.DELETE, endpoint + "/animals/delete/{id}")
+                                                .hasRole("ADMIN")
+                                                .anyRequest().authenticated())
+                                .userDetailsService(jpaUserDetailsService)
+                                .httpBasic(withDefaults())
+                                .sessionManagement(session -> session
+                                                .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED));
 
                 http.headers(header -> header.frameOptions(frame -> frame.sameOrigin()));
 
                 return http.build();
         }
 
+        // @Bean
+        // CorsConfigurationSource corsConfiguration() {
+        // CorsConfiguration configuration = new CorsConfiguration();
+        // configuration.setAllowCredentials(true);
+        // configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        // configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT",
+        // "DELETE"));
+        // configuration.setAllowedHeaders(Arrays.asList("*"));
+        // UrlBasedCorsConfigurationSource source = new
+        // UrlBasedCorsConfigurationSource();
+        // source.registerCorsConfiguration("/**", configuration);
+        // return source;
+        // }
+
         @Bean
-        CorsConfigurationSource corsConfiguration() {
+        public CorsConfigurationSource corsConfigurationSource() {
                 CorsConfiguration configuration = new CorsConfiguration();
                 configuration.setAllowCredentials(true);
-                configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
-                configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+                configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173")); // Asegúrate de que este origen
+                                                                                         // es el correcto
+                configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+                configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With"));
                 UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
                 source.registerCorsConfiguration("/**", configuration);
                 return source;
@@ -82,26 +106,32 @@ public class SecurityConfig {
                 return new Base64Encoder();
         }
 
-        /* @Bean
-        public InMemoryUserDetailsManager userDetailsManager() {
-
-                UserDetails mickey = User.builder()
-                                .username("mickey")
-                                .password("{bcrypt}$2a$12$8LegtLQWe717tIPvZeivjuqKnaAs5.bm0Q05.5GrAmcKzXw2NjoUO") // password
-                                .roles("USER")
-                                .build();
-
-                UserDetails minnie = User.builder()
-                                .username("minnie")
-                                .password("{bcrypt}$2a$12$8LegtLQWe717tIPvZeivjuqKnaAs5.bm0Q05.5GrAmcKzXw2NjoUO") // password
-                                .roles("ADMIN")
-                                .build();
-
-                Collection<UserDetails> users = new ArrayList<>();
-                users.add(mickey);
-                users.add(minnie);
-
-                return new InMemoryUserDetailsManager(users);
-        } */
+        /*
+         * @Bean
+         * public InMemoryUserDetailsManager userDetailsManager() {
+         * 
+         * UserDetails mickey = User.builder()
+         * .username("mickey")
+         * .password(
+         * "{bcrypt}$2a$12$8LegtLQWe717tIPvZeivjuqKnaAs5.bm0Q05.5GrAmcKzXw2NjoUO") //
+         * password
+         * .roles("USER")
+         * .build();
+         * 
+         * UserDetails minnie = User.builder()
+         * .username("minnie")
+         * .password(
+         * "{bcrypt}$2a$12$8LegtLQWe717tIPvZeivjuqKnaAs5.bm0Q05.5GrAmcKzXw2NjoUO") //
+         * password
+         * .roles("ADMIN")
+         * .build();
+         * 
+         * Collection<UserDetails> users = new ArrayList<>();
+         * users.add(mickey);
+         * users.add(minnie);
+         * 
+         * return new InMemoryUserDetailsManager(users);
+         * }
+         */
 
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,34 @@
+-- Insertando datos en la tabla `families`
+INSERT INTO families (name) VALUES 
+('Felidae'), 
+('Canidae'),  
+('Reptilia'), 
+('Mustelidae'),
+('Leporidae'); 
+
+-- Insertando datos en la tabla `types`
+INSERT INTO types (name, family_id) VALUES 
+('Lion', (SELECT id FROM families WHERE name = 'Felidae')), 
+('Tiger', (SELECT id FROM families WHERE name = 'Felidae')),
+('Wolf', (SELECT id FROM families WHERE name = 'Canidae')),
+('Fox', (SELECT id FROM families WHERE name = 'Canidae')),
+('Crocodile', (SELECT id FROM families WHERE name = 'Reptilia')),
+('Snake', (SELECT id FROM families WHERE name = 'Reptilia')),
+('Otter', (SELECT id FROM families WHERE name = 'Mustelidae')),
+('Weasel', (SELECT id FROM families WHERE name = 'Mustelidae')),
+('Rabbit', (SELECT id FROM families WHERE name = 'Leporidae')),
+('Hare', (SELECT id FROM families WHERE name = 'Leporidae'));
+
+-- -- Insertando datos en la tabla `animals`
+INSERT INTO animals (name, type_id, gender, date_of_entry) VALUES 
+('Simba', (SELECT id FROM types WHERE name = 'Lion'), 'Male', '2022-01-15'),
+('Nala', (SELECT id FROM types WHERE name = 'Lion'), 'Female', '2022-03-10'),
+('Shere Khan', (SELECT id FROM types WHERE name = 'Tiger'), 'Male', '2021-07-25'),
+('Akela', (SELECT id FROM types WHERE name = 'Wolf'), 'Male', '2020-12-05'),
+('Vixen', (SELECT id FROM types WHERE name = 'Fox'), 'Female', '2021-09-19'),
+('Nagini', (SELECT id FROM types WHERE name = 'Snake'), 'Female', '2019-05-14'),
+('Crocky', (SELECT id FROM types WHERE name = 'Crocodile'), 'Male', '2018-04-22'),
+('Pippin', (SELECT id FROM types WHERE name = 'Otter'), 'Female', '2023-02-17'),
+('Whiskers', (SELECT id FROM types WHERE name = 'Weasel'), 'Male', '2023-01-05'),
+('Thumper', (SELECT id FROM types WHERE name = 'Rabbit'), 'Male', '2022-10-29'),
+('Bambi', (SELECT id FROM types WHERE name = 'Hare'), 'Female', '2023-06-12');
