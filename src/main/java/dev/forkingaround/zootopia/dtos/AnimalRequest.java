83fd29557980f6d1177d846ed60f58113cca86c1
Miharu669 <veronicadoelfuentes@gmail.com>
2024-08-13 12:31:10 +0200

    came back from hell, fixed AnimalController, need to do the test again

---
+++ src/main/java/dev/forkingaround/zootopia/dtos/AnimalRequest.java
@@ -0,0 +1,13 @@
+package dev.forkingaround.zootopia.dtos;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class AnimalRequest {
+    private String name;
+    private String typeName;
+    private String gender;
+    private Date dateOfEntry;
+}
